6042669d7d29d4f6240f6d3fc8860cb521999d32
Thomas <thomasfaller@gmail.com>
2017-06-08 09:58:07 +0100

    improvement with hover texts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(()=>{
-  $('#searchForm').on('submit', (e) => {
+  $('.field-wrap').on('submit','#searchForm', (e) => {
     let $searchText = $('#searchText').val();
+    $('#movies').empty();
     getMovies($searchText);
     e.preventDefault();
   });
@@ -13,8 +14,15 @@ function getMovies(search) {
     'https://api.themoviedb.org/3/search/movie?api_key=c082d31257497077fd3ba0047bbd6358&query=' + search)
     .then((response) => {
         for (var i = 0; i < response.data.results.length ; i++ ) {
-          $('#movies').append('<div class="col-4 m-0 container"><img class="img-fluid" src="https://image.tmdb.org/t/p/w500' + response.data.results[i].poster_path + '"></img><figcaption>' + response.data.results[i].original_title + '</figcaption></div>')
-        }e
+          var $title = response.data.results[i].original_title;
+          var $description = response.data.results[i].overview;
+          var $posterUrl = 'https://image.tmdb.org/t/p/w500' + response.data.results[i].poster_path;
+          var $noPoster = 'img/dvd_cover.png';
+            if (response.data.results[i].poster_path == null) {
+            $('.wrapper').append('<div class="cards" style="background-image: url(' + $noPoster + ')"><div class="hover_text"><h3>' + $title + '</h3><p>' + $description + '</p><button>More Info</button></div></div>')
+              } else {
+              $(.wrapper).append('<div class="cards" style="background-image: url(' + $posterUrl + ')"><div class="hover_text"><h3>' + $title + '</h3><p>' + $description + '</p><button>More Info</button></div></div>')
+              }}
     })
     .catch((err) => {
       console.log(err);
