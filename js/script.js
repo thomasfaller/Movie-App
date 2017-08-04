5def4e1f3d36a9342a60dad150617c128cd93a6a
Thomas Faller <thomasfaller@gmail.com>
2017-08-04 12:34:34 +0100

    Changed CSS
    Added hover functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 
   $(document).on('submit','#searchForm', (e) => {
     $('.wrapper').empty();
@@ -21,10 +22,15 @@ function getMovies(search) {
           var $link = 'https://www.themoviedb.org/movie/' + response.data.results[i].id;
           var $rating = response.data.results[i].vote_average;
           var $ratingColor = '#57e32c';
+          var length = 250;    // This is the max length of any description
+          var $trimmedDescription = $description.substring(0, length) + '...';
 
           // rating color generator
 
-          if ($rating <= 4) {
+          if ($rating == 0) {
+          $ratingColor = '#a7a7a7';
+          $rating = 'NA';
+          } else if ($rating <= 4) {
             $ratingColor = '#ff4545';
           } else if ($rating <= 5) {
             $ratingColor = '#ffa534';
@@ -39,9 +45,9 @@ function getMovies(search) {
 
 
             if (response.data.results[i].poster_path == null) {
-            $('.wrapper').append('<div class="cards" style="background-image: url(' + $noPoster + ')"><div class="hover_text"><h3>' + $title + '</h3><span style="background-color:' + $ratingColor + '">' + $rating + '</span><p>' + $description + '</p><a target="_blank" href=' + $link + ' class="btn btn-info" role="button">More Info</a></div></div>')
+            $('.wrapper').append('<div class="cards" style="background-image: url(' + $noPoster + ')"><div class="hover_text"><h3>' + $title + '</h3><span style="background-color:' + $ratingColor + '">' + $rating + '</span><p>' + $trimmedDescription + '</p><a target="_blank" href=' + $link + ' class="btn btn-info" role="button">More Info</a></div></div>')
               } else {
-              $('.wrapper').append('<div class="cards" style="background-image: url(' + $posterUrl + ')"><div class="hover_text"><h3>' + $title + '</h3><span style="background-color:' + $ratingColor + '">' + $rating + '</span><p>' + $description + '</p><a target="_blank" href=' + $link + ' class="btn btn-info " role="button">More Info</a></div></div>')
+              $('.wrapper').append('<div class="cards" style="background-image: url(' + $posterUrl + ')"><div class="hover_text"><h3>' + $title + '</h3><span style="background-color:' + $ratingColor + '">' + $rating + '</span><p>' + $trimmedDescription + '</p><a target="_blank" href=' + $link + ' class="btn btn-info " role="button">More Info</a></div></div>')
               }}
     })
     .catch((err) => {
